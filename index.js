f747df9043e25ab2867537babe0b86f6b5585456
Probbes <antoinem1706@yahoo.fr>
2022-09-23 17:28:24 +0200

    New website
    Just to see if the new website is working

---
+++ index.js
@@ -0,0 +1,10 @@
+// document.getElementById("idButtonHome").onclick = function () {
+	// location.href = "index.html"; 
+// };
+
+function toCV(i){
+	if (i==1) { location.href = "index.html"; }
+	else if (i==2) { location.href = "CV.html"; }
+	else if (i==3) { location.href = "portfolio.html"; }
+	else if (i==3) { location.href = "CV.html"; }
+}
