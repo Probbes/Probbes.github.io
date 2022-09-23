8abcfb9095a0fab7f9ad10a2b65c929253a8f4ae
Probbes <antoinem1706@yahoo.fr>
2022-09-23 17:33:24 +0200

    changed Accueil

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 	// location.href = "index.html"; 
 // };
 
-function toCV(i){
+function funcButton(i){
 	if (i==1) { location.href = "index.html"; }
 	else if (i==2) { location.href = "cv.html"; }
 	else if (i==3) { location.href = "portfolio.html"; }
