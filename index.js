aeacceed57f24945148797bdce5625a739ff37e4
Probbes <antoinem1706@yahoo.fr>
2022-09-23 17:30:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 function toCV(i){
 	if (i==1) { location.href = "index.html"; }
-	else if (i==2) { location.href = "CV.html"; }
+	else if (i==2) { location.href = "cv.html"; }
 	else if (i==3) { location.href = "portfolio.html"; }
-	else if (i==3) { location.href = "CV.html"; }
+	else if (i==3) { location.href = "cv.html"; }
 }
